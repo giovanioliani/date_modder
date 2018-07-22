970cbfb8ddf9f62d3f261dc12c9c8adc1e38bc83
Giovani Oliani <giovani.oliani@gmail.com>
2018-07-22 16:00:49 -0300

    Add missing slash if necessary, remove control characters from date line

--- mod_date.py
+++ mod_date.py
@@ -8,16 +8,19 @@ import sys
 import datetime
 import os
 import calendar
+from re import sub
 
 if len(sys.argv) < 2:
 	print "Usage: python mod_date.py [folder_name]"
 	exit(1)
 
 for filename in os.listdir(sys.argv[1]):
+	if sys.argv[1][-1] != '/':
+		sys.argv[1] = sys.argv[1] + '/'
 	file = open(sys.argv[1] + filename, "r")
 	data = file.readline()
 	file.close()
-	data = data.split("/")
+	data = sub("[^0-9/]", "", data).split("/")
 	seconds = calendar.timegm(datetime.datetime(int(data[2]), int(data[1]), int(data[0]), 0, 0).timetuple())
 	os.utime(sys.argv[1] + filename, (seconds,seconds))
 
